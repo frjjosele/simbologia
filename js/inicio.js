4e5ccc58c0cdafd9f4e92ab4372d324b7ee3756c
francisco jose cuestas <cuestasfranciscojose@gmail.com>
2020-08-09 20:04:49 +0200

    agregando todo el html sin estilos aun, solo 2 datos

--- js/inicio.js
+++ js/inicio.js
@@ -1,17 +1,21 @@
-$selectSimbologia=document.getElementById("simbolos");
-const $demo=document.getElementById("demo");
+$selectSimbologia=document.getElementById("selecSimbolos");
+/* const $sectionSimbolos=document.getElementById("simbolos");
+ */
+const $carouselItemMecanismo=document.getElementById("carousel-item-mecanismo");
+const $carouselitemMultifilar=document.getElementById("carousel-item-multifilar");
+const $carouselitemUnifilar=document.getElementById("carousel-item-unifilar");
+const $carouselItemCondiciones=document.getElementById("carousel-item-condiciones");
 function loadDoc(event){
-  console.log(event);
- 
-  $demo.innerHTML=`<img src=${simbolos[event].mecanismo}></img>
+  console.log($carouselItemMecanismo.firstElementChild.src);
+  console.log(simbolos[event].mecanismo);
+  $carouselItemMecanismo.firstElementChild.src=simbolos[event].mecanismo;
+  $carouselitemUnifilar.firstElementChild.src=simbolos[event].unifilar;
+  $carouselitemMultifilar.firstElementChild.src=simbolos[event].multifilar;
+  console.log($carouselItemCondiciones.firstElementChild);
+  $carouselItemCondiciones.firstElementChild.textContent=simbolos[event].condiciones;
+/*   $sectionSimbolos.innerHTML=`<img src=${simbolos[event].mecanismo}></img>
                     <img src=${simbolos[event].unifilar}></img>
                     <img src=${simbolos[event].multifilar}></img>
                     <p>Descripción ${simbolos[event].descriptcion}</p>
-                    <p>Descripción ${simbolos[event].condiciones}</p>`;
+                    <p>Descripción ${simbolos[event].condiciones}</p>`; */
 }
-
-/* "mecanismo":"../images/mecanismo/interruptor.png",
-     "unifilar":"../images/unifilar/interruptorUni.png",
-     "Multifilar":"../images/multifilar/interruptorMulti.png",
-     "descriptcion":"Interruptor",
-     "Condiciones":"Empotrado en caja de mecanismo a una altura de 110 cm de pavimento y 15 cm del marco de la puerta (a excepción de  cabeceros en dormitorios). A derecha o izquierda de éste pero siempre en el mismo lado del mecanismo de apertura de la puerta. Se prestará especial interés en la correcta fijación de la caja de mecanismo, debiendo estar nivelada y enrasada, de forma que permita que la placa de los mecanismos queden perfectamente adosados al paramento. Los mecanismos deberán interrumpir la fase."     */
